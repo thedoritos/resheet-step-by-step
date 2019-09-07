5617bb8fa2b7e3d234f02b394fccfa0165acb6b9
thedoritos <thedoritos@gmail.com>
2019-09-07 22:57:46 +0900

    Add sheet

--- resheet/app.rb
+++ resheet/app.rb
@@ -1,20 +1,27 @@
 require 'googleauth'
 require 'google/apis/sheets_v4'
+require 'json'
 
 module Resheet
   class App
     def self.call(env)
       credentials = Google::Auth::ServiceAccountCredentials.make_creds(
         json_key_io: File.open('secrets/credentials.json'),
-	scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS
+        scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS
       )
       credentials.fetch_access_token!
 
       service = Google::Apis::SheetsV4::SheetsService.new
       service.authorization = credentials
 
-      values = service.get_spreadsheet_values(ENV['SPREADSHEET_ID'], 'animations!A:Z').values
-      [200, { 'Content-Type' => 'application/json' }, [values.to_s]]
+      sheet = Resheet::Sheet.new(service, ENV['SPREADSHEET_ID'], 'animations')
+      sheet.fetch
+
+      if error = sheet.error
+        return [500, {}, []]
+      end
+
+      [200, { 'Content-Type' => 'application/json' }, [JSON.generate(sheet.records)]]
     end
   end
 end
---
+++ resheet/sheet.rb
@@ -0,0 +1,28 @@
+module Resheet
+  class Sheet
+    attr_reader :header, :records
+    attr_reader :error
+
+    def initialize(google_sheet_service, spreadsheet_id, resource)
+      @google_sheet_service = google_sheet_service
+      @spreadsheet_id = spreadsheet_id
+      @resource = resource
+    end
+
+    def fetch
+      values = @google_sheet_service
+        .get_spreadsheet_values(@spreadsheet_id, "#{@resource}!A:Z")
+        .values
+
+      @header = values.first
+      @records = values.drop(1)
+        .map do |row|
+          @header.each_with_index.map { |key, i| [key, row[i]] }.to_h
+        end
+
+    rescue Google::Apis::ClientError => error
+      @error = error
+    end
+  end
+end
+
