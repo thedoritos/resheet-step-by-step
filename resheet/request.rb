0831e2761d7b70bed04f11fdc485bf45c44870c0
thedoritos <thedoritos@gmail.com>
2019-09-07 19:33:24 +0900

    Add request

---
+++ resheet/request.rb
@@ -0,0 +1,16 @@
+module Resheet
+  class Request
+    attr_reader :method, :resource, :id, :params
+
+    def initialize(env)
+      rack_request = Rack::Request.new(env)
+      path_components = rack_request.path_info.split('/')
+
+      @method = rack_request.request_method
+      @resource = path_components[1]
+      @id = path_components[2] || rack_request.params['id']
+      @params = rack_request.params.merge({ 'id' => @id })
+    end
+  end
+end
+
